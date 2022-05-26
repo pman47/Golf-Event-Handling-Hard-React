6588a9016f540d2ff219196f91a181e8a7d5a510
Manish <manishprajapatim416@gmail.com>
2022-05-26 18:43:03 +0530

    Something done 👍

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect, useCallback } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -9,48 +9,48 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
+
   const start = () => {
     const start = document.querySelector(".start");
     start.classList.remove("start");
     start.classList.add("ball");
-    window.addEventListener("keydown", (event) => {
-      if (event.keyCode === 37) {
-        //Left
-        console.log("left");
-        // xBall(-5);
-        let tmp = x - 5;
-        console.log(tmp);
-        setX(tmp);
-        console.log(x);
-      } else if (event.keyCode === 39) {
-        //Right
-        xBall(5);
-      } else if (event.keyCode === 38) {
-        //Up
-        yBall(-5);
-      } else if (event.keyCode === 40) {
-        //Down
-        yBall(5);
-      }
-      setBallPosition(x, y);
-      // console.log(x, y);
-    });
+    start.innerHTML = "";
   };
 
-  function xBall(offset) {
-    setX(x + offset);
-  }
+  const keyHandler = (event) => {
+    if (event.keyCode === 37) {
+      setX(x - 5);
+    } else if (event.keyCode === 39) {
+      setX(x + 5);
+    } else if (event.keyCode === 38) {
+      setY(y - 5);
+    } else if (event.keyCode === 40) {
+      setY(y + 5);
+    }
+    let ballPositionClone = { ...ballPosition };
+    ballPositionClone.left = x + "px";
+    ballPositionClone.top = y + "px";
+    setBallPosition(ballPositionClone);
+    // console.log(ballPosition);
+  };
 
-  function yBall(offset) {
-    setY(y + offset);
-  }
+  useEffect(() => {
+    // console.log(x);
+    // console.log(y);
+    window.addEventListener("keydown", keyHandler);
+    return () => window.removeEventListener("keydown", keyHandler);
+  });
 
   const reset = () => {};
   const renderChoice = () => {};
 
   return (
     <div className="playground">
-      <button onClick={start} className="start">
+      <button
+        onClick={start}
+        style={{ left: ballPosition.left, top: ballPosition.top }}
+        className="start"
+      >
         Start
       </button>
       <button onClick={reset} className="reset">
