a54d1c7b26848183b117dd9cdc613a68921ce181
Manish <manishprajapatim416@gmail.com>
2022-05-26 19:11:55 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -15,33 +15,41 @@ const App = () => {
   };
 
   const keyHandler = (event) => {
-    if (event.keyCode === 37) {
-      setX(x - 5);
-    } else if (event.keyCode === 39) {
-      setX(x + 5);
-    } else if (event.keyCode === 38) {
-      setY(y - 5);
-    } else if (event.keyCode === 40) {
-      setY(y + 5);
-    }
     let ballPositionClone = { ...ballPosition };
-    ballPositionClone.left = x + "px";
-    ballPositionClone.top = y + "px";
+
+    if (event.keyCode === 37) {
+      ballPositionClone.left =
+        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) -
+        5 +
+        "px";
+    } else if (event.keyCode === 39) {
+      ballPositionClone.left =
+        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) +
+        5 +
+        "px";
+    } else if (event.keyCode === 38) {
+      ballPositionClone.top =
+        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) -
+        5 +
+        "px";
+    } else if (event.keyCode === 40) {
+      ballPositionClone.top =
+        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) +
+        5 +
+        "px";
+    }
+    // ballPositionClone.left = (Number(ballPosition.left.slice(0,ballPosition.left.indexOf("p")))+5)+"px";
     setBallPosition(ballPositionClone);
     // console.log(ballPosition);
   };
 
   useEffect(() => {
-    console.log(x);
-    console.log(y);
     window.addEventListener("keydown", keyHandler);
     return () => window.removeEventListener("keydown", keyHandler);
   });
 
   const reset = () => {
     setRenderBall(!renderBall);
-    setX(0);
-    setY(0);
     setBallPosition({ left: "0px", top: "0px" });
   };
   const renderChoice = () => {};
